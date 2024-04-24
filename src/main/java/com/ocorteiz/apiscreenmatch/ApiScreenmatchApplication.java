415324c75d7ae8ce22a54c52daa4cbd0634c7b1a
Luis Cortêz Dell <112598652+ocorteiz@users.noreply.github.com>
2024-04-24 11:14:29 -0300

    creating record dadosEpisode
    manipulates dadosEpisode using stream

--- src/main/java/com/ocorteiz/apiscreenmatch/ApiScreenmatchApplication.java
+++ src/main/java/com/ocorteiz/apiscreenmatch/ApiScreenmatchApplication.java
@@ -1,17 +1,9 @@
 package com.ocorteiz.apiscreenmatch;
 
 import com.ocorteiz.apiscreenmatch.main.Principal;
-import com.ocorteiz.apiscreenmatch.model.DadosEpisodio;
-import com.ocorteiz.apiscreenmatch.model.DadosSerie;
-import com.ocorteiz.apiscreenmatch.model.DadosTemporada;
-import com.ocorteiz.apiscreenmatch.service.ConsumirApi;
-import com.ocorteiz.apiscreenmatch.service.Converterdados;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class ApiScreenmatchApplication implements CommandLineRunner {
--- src/main/java/com/ocorteiz/apiscreenmatch/main/Principal.java
+++ src/main/java/com/ocorteiz/apiscreenmatch/main/Principal.java
@@ -1,13 +1,16 @@
 package com.ocorteiz.apiscreenmatch.main;
 
+import com.ocorteiz.apiscreenmatch.model.DadosEpisodio;
 import com.ocorteiz.apiscreenmatch.model.DadosSerie;
 import com.ocorteiz.apiscreenmatch.model.DadosTemporada;
 import com.ocorteiz.apiscreenmatch.service.ConsumirApi;
 import com.ocorteiz.apiscreenmatch.service.Converterdados;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Principal {
 
@@ -17,23 +20,34 @@ public class Principal {
     private ConsumirApi consumirApi = new ConsumirApi();
     private Converterdados converterdados = new Converterdados();
 
-    public void exibirMenu(){
+    public void exibirMenu() {
         System.out.println("DIGITE O NOME DA SERIE: ");
         var nomeSerie = leitura.nextLine();
 
-        var json = consumirApi.obterDados( ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
+        var json = consumirApi.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
         DadosSerie dadosSerie = converterdados.obterDados(json, DadosSerie.class);
-        System.out.println(dadosSerie+"\n");
+        System.out.println(dadosSerie + "\n");
 
         List<DadosTemporada> temporadaList = new ArrayList<>();
 
-        for(int i = 1 ; i <= dadosSerie.totalTemporadas() ; i++){
+        for (int i = 1; i <= dadosSerie.totalTemporadas(); i++) {
             json = consumirApi.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + "&season=" + i + API_KEY);
             DadosTemporada dadosTemporada = converterdados.obterDados(json, DadosTemporada.class);
             temporadaList.add(dadosTemporada);
         }
 
         temporadaList.forEach(t -> t.episodios().forEach(e -> System.out.println(e.titulo())));
+
+        List<DadosEpisodio> episodioList = temporadaList.stream()
+                .flatMap(t -> t.episodios().stream())
+                .collect(Collectors.toList());
+
+        System.out.println("\nTOP 5 EPISODIOS:");
+        episodioList.stream()
+                .filter(e -> !e.avaliacao().equalsIgnoreCase("N/A"))
+                .sorted(Comparator.comparing(DadosEpisodio::avaliacao).reversed())
+                .limit(5)
+                .forEach(System.out::println);
     }
 
 }
